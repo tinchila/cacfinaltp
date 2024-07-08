fb2aee3a6d787ea5d08076018ee5ec739bcfef6c
Martin <martin_dotto@hotmail.com>
2024-07-08 20:06:25 -0300

    Deploy of web project with data base 13

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, jsonify, send_from_directory
-from flask_mysqldb import MySQL
 from flask_cors import CORS
 import os
 from dotenv import load_dotenv
+from database import DbSession
 
 load_dotenv()
 
@@ -11,17 +11,9 @@ CORS(app, resources={r"/*": {"origins": "*"}})
 env = os.getenv('FLASK_ENV')
 
 if env == 'production':
-    app.config['MYSQL_HOST'] = os.getenv('PROD_MYSQL_HOST')
-    app.config['MYSQL_USER'] = os.getenv('PROD_MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.getenv('PROD_MYSQL_PASSWORD')
-    app.config['MYSQL_DB'] = os.getenv('PROD_MYSQL_DB')
+    db_url = f"postgresql://{os.getenv('PROD_MYSQL_USER')}:{os.getenv('PROD_MYSQL_PASSWORD')}@{os.getenv('PROD_MYSQL_HOST')}/{os.getenv('PROD_MYSQL_DB')}"
 else:
-    app.config['MYSQL_HOST'] = os.getenv('LOCAL_MYSQL_HOST')
-    app.config['MYSQL_USER'] = os.getenv('LOCAL_MYSQL_USER')
-    app.config['MYSQL_PASSWORD'] = os.getenv('LOCAL_MYSQL_PASSWORD')
-    app.config['MYSQL_DB'] = os.getenv('LOCAL_MYSQL_DB')
-
-mysql = MySQL(app)
+    db_url = f"mysql://{os.getenv('LOCAL_MYSQL_USER')}:{os.getenv('LOCAL_MYSQL_PASSWORD')}@{os.getenv('LOCAL_MYSQL_HOST')}/{os.getenv('LOCAL_MYSQL_DB')}"
 
 # Guardar imágenes subidas
 UPLOAD_FOLDER = 'path_to_save_images'
@@ -29,8 +21,6 @@ if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
 
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-mysql = MySQL(app)
 
 # Ruta para servir archivos estáticos
 @app.route('/path_to_save_images/<path:filename>')
@@ -40,21 +30,20 @@ def serve_image(filename):
 # Ruta Obtener todas las bebidas
 @app.route('/api/bebidas', methods=['GET'])
 def get_bebidas():
-    cur = mysql.connection.cursor()
-    cur.execute('SELECT * FROM bebidas')
-    data = cur.fetchall()
-    bebidas = []
-    for row in data:
-        bebidas.append({
-            'id': row[0],
-            'bebida': row[1],
-            'marca': row[2],
-            'variedad': row[3],
-            'precio': row[4],
-            'imagen': f"{request.host_url}{app.config['UPLOAD_FOLDER']}/{row[5]}",
-            'cantidad': row[6]
-        })
-    cur.close()
+    with DbSession(db_url) as db:
+        db.execute('SELECT * FROM bebidas')
+        data = db.fetchall()
+        bebidas = []
+        for row in data:
+            bebidas.append({
+                'id': row['id'],
+                'bebida': row['bebida'],
+                'marca': row['marca'],
+                'variedad': row['variedad'],
+                'precio': row['precio'],
+                'imagen': f"{request.host_url}{app.config['UPLOAD_FOLDER']}/{row['imagen']}",
+                'cantidad': row['cantidad']
+            })
     return jsonify(bebidas)
 
 # Ruta Crear una nueva bebida
@@ -70,13 +59,11 @@ def add_bebida():
     imagen_filename = imagen.filename
     imagen.save(os.path.join(app.config['UPLOAD_FOLDER'], imagen_filename))
 
-    cur = mysql.connection.cursor()
-    cur.execute('INSERT INTO bebidas (bebida, marca, variedad, precio, imagen, cantidad) VALUES (%s, %s, %s, %s, %s, %s)', 
-                (bebida, marca, variedad, precio, imagen_filename, cantidad))
-    mysql.connection.commit()
-    cur.close()
+    with DbSession(db_url) as db:
+        db.execute('INSERT INTO bebidas (bebida, marca, variedad, precio, imagen, cantidad) VALUES (%s, %s, %s, %s, %s, %s)', 
+                   (bebida, marca, variedad, precio, imagen_filename, cantidad))
+        db.commit()
     return jsonify({'message': 'Bebida creada'}), 201
-
 
 # Ruta Actualizar una bebida existente
 @app.route('/api/bebidas/<int:id>', methods=['PUT'])
@@ -91,20 +78,18 @@ def update_bebida(id):
     imagen_filename = imagen.filename
     imagen.save(os.path.join(app.config['UPLOAD_FOLDER'], imagen_filename))
 
-    cur = mysql.connection.cursor()
-    cur.execute('UPDATE bebidas SET bebida=%s, marca=%s, variedad=%s, precio=%s, imagen=%s, cantidad=%s WHERE id=%s', 
-                (bebida, marca, variedad, precio, imagen_filename, cantidad, id))
-    mysql.connection.commit()
-    cur.close()
+    with DbSession(db_url) as db:
+        db.execute('UPDATE bebidas SET bebida=%s, marca=%s, variedad=%s, precio=%s, imagen=%s, cantidad=%s WHERE id=%s', 
+                   (bebida, marca, variedad, precio, imagen_filename, cantidad, id))
+        db.commit()
     return jsonify({'message': 'Bebida actualizada'}), 200
 
 #Ruta Borrar una bebida
 @app.route('/api/bebidas/<int:id>', methods=['DELETE'])
 def delete_bebida(id):
-    cur = mysql.connection.cursor()
-    cur.execute('DELETE FROM bebidas WHERE id=%s', (id,))
-    mysql.connection.commit()
-    cur.close()
+    with DbSession(db_url) as db:
+        db.execute('DELETE FROM bebidas WHERE id=%s', (id,))
+        db.commit()
     return jsonify({'message': 'Bebida eliminada'}), 200
 
 # Ruta para la página principal
