d3e9f5959825f463653aa68b601fb2c5212c07f6
Martin <martin_dotto@hotmail.com>
2024-07-09 23:14:42 -0300

    Repo a nivel local que funciona 2

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+# change this line for whichever package you use, such as pip, or poetry, etc.
+pip install -r requirements.txt
+poetry install
---
+++ frontend/Dockerfile
@@ -0,0 +1,7 @@
+# frontend/Dockerfile
+FROM nginx:alpine
+
+COPY . /usr/share/nginx/html
+
+EXPOSE 80
+CMD ["nginx", "-g", "daemon off;"]
